c5d8245d2d1102a71761f0905d4f2cee295680b5
AnatKordon <blabla1107@gmail.com>
2025-04-19 13:23:49 +0200

    HW completed

--- hw2_q1.py
+++ hw2_q1.py
@@ -1,3 +1,5 @@
+import re
+
 MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
               'D': '-..',    'E': '.',      'F': '..-.',
               'G': '--.',    'H': '....',   'I': '..',
@@ -19,108 +21,10 @@ MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
 
 
 
+###3rd attempt without for:
 def english_to_morse(
     input_file: str = "lorem.txt",
     output_file: str = "lorem_morse.txt"
-):
-    """Convert an input text file to an output Morse code file.
-
-    Notes
-    -----
-    This function assumes the existence of a MORSE_CODE dictionary, containing a
-    mapping between English letters and their corresponding Morse code.
-
-    Parameters
-    ----------
-    input_file : str
-        Path to file containing the text file to convert.
-    output_file : str
-        Name of output file containing the translated Morse code. Please don't change
-        it since it's also hard-coded in the tests file.
-    """
-    with open(input_file, "r") as input:
-        content = input.read().upper()
-
-    # Split text by paragraphs
-    paragraphs = content.split("\n\n")
-    output_lines = []
-
-    for paragraph in paragraphs:
-        words = paragraph.split()
-        for word in words:
-            morse_word = ""
-            for letter in word:
-                # if letter in MORSE_CODE: #add raise error if letter isn't in morse because right now it can miss things
-                morse_word += MORSE_CODE[letter]
-            output_lines.append(morse_word)
-        output_lines.append("")  # empty line between paragraphs
-
-    output_string = '\n'.join(output_lines)
-
-    with open(output_file, "w") as output:
-        output.write(output_string)
-
-    return output_string
-
-
-output_string = english_to_morse(
-    input_file = "lorem.txt",
-    output_file = "lorem_morse.txt")
-
-
-#####2nd attempt:
-import re
-def text_to_morse_lines(
-    input_file: str = "lorem.txt",
-    output_file: str = "lorem_morse1.txt"
-):
-    """Convert an input text file to an output Morse code file.
-
-    Notes
-    -----
-    This function assumes the existence of a MORSE_CODE dictionary, containing a
-    mapping between English letters and their corresponding Morse code.
-
-    Parameters
-    ----------
-    input_file : str
-        Path to file containing the text file to convert.
-    output_file : str
-        Name of output file containing the translated Morse code. Please don't change
-        it since it's also hard-coded in the tests file.
-    """
-    with open(input_file, "r") as input:
-        content = input.read().upper()
-
-    
-    # Split on words AND capture the empty lines (double newlines)
-    parts = re.split(r'(\s+)', content)  # splits by a single space - to seperate words, and also double space for paragraphs
-    morse_lines = []
-
-    for part in parts:
-        if part.isspace(): 
-            if '\n' in part:
-                # If it contains a newline, treat it as a paragraph break
-                morse_lines.append('')
-        elif part:  # non-empty word
-            morse_word = ''.join(MORSE_CODE.get(c, '') for c in part) #turning english chars to morse code
-            morse_lines.append(morse_word)
-
-    output_string = '\n'.join(morse_lines)
-    
-    with open(output_file, "w") as output:
-        output.write(output_string)
-
-    return output_string
-
-output_string_2 = text_to_morse_lines(
-    input_file = "lorem.txt",
-    output_file = "lorem_morse1.txt")
-
-###3rd attempt without for:
-def translate_to_morse(
-    input_file: str = "lorem.txt",
-    output_file: str = "lorem_morse2.txt"
 ):
     """Convert an input text file to an output Morse code file.
 
@@ -148,7 +52,7 @@ def translate_to_morse(
     translated = content.translate(trans_table)
     temp_text = re.sub(r'\n\n+', '<break>', translated) #replace double newlines with a placeholder for them to differ from the spaces between words in next line 
     temp_text = re.sub(r'\s+', '\n', temp_text) #turn spaces to new lines in order to have each word in it's own line
-    output_string = temp_text.replace('<break>', '\n\n') #put back double newlines
+    output_string = temp_text.replace('<break>', '\n\n').rstrip('\n') #put back double newlines and remove final newline
 
     try:
         with open(output_file, "w") as output:
@@ -159,8 +63,9 @@ def translate_to_morse(
         print("succesfull execution!")
 
     return output_string
-if __name__ == __main__:
-    output_string_3 = translate_to_morse(
+
+if __name__ == '__main__':
+    output_string = english_to_morse(
         input_file = "lorem.txt",
-        output_file = "lorem_morse2.txt")
+        output_file = "lorem_morse.txt")
 
--- hw2_q2.py
+++ hw2_q2.py
@@ -28,25 +28,24 @@ def meetup(agent_listing: tuple) -> list:
     updated_listing : list
         A list of Agents with their 'category' field changed according to the result
         of the meeting.
-    """
-
-    """I will be using condition values instead of names (Condition.value):
+    
+    Anat's Note: 
+    I will be using condition values instead of names (Condition.value):
         CURE 1
         HEALTHY 2
         SICK 3
         DYING 4
         DEAD 5
         """
-    
+    #calling other functions:
     healthy_dead_list, meetings_list = spliting_agents(agent_listing)
     updated_healthy_dead_list, updated_meetings_list = uneven_meetings(healthy_dead_list, meetings_list)
     updated_listing = updated_healthy_dead_list
-    #print(f"discarded_agents: {healthy_dead_list}, pepole who meet: {meetings_list}")
-
+   
     if len(updated_meetings_list) == 0:
         return updated_listing
     else:
-        for agent1, agent2 in batched(updated_meetings_list, 2): 
+        for agent1, agent2 in batched(updated_meetings_list, 2): #batched is a method from itertools that batches tuples
         #for agent1, agent2 in zip(agent_listing[::2], agent_listing[1::2]):
             val1, val2 = agent1.category.value, agent2.category.value #extracting values which i'll check for every condition
             ##if healthy or dead - there's no encounter so they stay the same
@@ -56,7 +55,7 @@ def meetup(agent_listing: tuple) -> list:
                 if val1 == 1 and val2 > 1:
                     updated_listing.extend([
                     agent1,
-                    agent2._replace(category=Condition(val2 - 1))
+                    agent2._replace(category=Condition(val2 - 1)) #replace changes the value of the agent's category
                 ])
                 elif val2 == 1 and val1 > 1:
                     updated_listing.extend([
@@ -72,11 +71,26 @@ def meetup(agent_listing: tuple) -> list:
                 updated_listing.extend([agent1])
             else: 
                 print(f"you didn't think about this case: {agent1}, {agent2}")
-            #print(f"updated: {updated_listing}")
     return updated_listing
 
 
 def spliting_agents(agent_listing: tuple):
+    """splits agents to create a new meetings list that has only agents that will meet,
+    also save the agents who don't meet
+    Parameters
+    ----------
+    agent_listing : tuple
+        A listing (tuple in this case) in which each element is of the Agent
+        type, containing a 'name' field and a 'category' field, with 'category' being
+        of the type Condition.
+
+    Returns
+    -------
+    healthy_dead_list : list
+        A list of agents who are healthy or dead (they don't meet).
+    meetings_list : list
+        A list of agents who will meet (category is cure, sick, or dying).
+    """
     healthy_dead_list = []
     meetings_list = []
     for agent in agent_listing:
@@ -88,10 +102,22 @@ def spliting_agents(agent_listing: tuple):
     return healthy_dead_list, meetings_list
 
 def uneven_meetings(healthy_dead_list, meetings_list):
+    """filtering solo agents from the meetings_list to make it even
+
+    Parameters
+    ----------
+    healthy_dead_list : list
+    meetings_list : list
+
+    Returns
+    -------
+    healthy_dead_list : list
+    meetings_list : list
+        returns updated versions of the two lists
+    """
     for agent in meetings_list:
         if len(meetings_list) % 2 != 0:
             healthy_dead_list.append(meetings_list[-1]) #append last agent
             meetings_list.remove(meetings_list[-1])
     return healthy_dead_list, meetings_list
-
 
